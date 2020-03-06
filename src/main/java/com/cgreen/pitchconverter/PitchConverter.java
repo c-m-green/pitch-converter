72d91e8436f8d1813a6af6afded2ad321cb5f1aa
Christopher Green <cgvoice35@gmail.com>
2020-03-05 20:40:18 -0600

    Up to version 0.6.0

--- src/main/java/com/cgreen/pitchconverter/PitchConverter.java
+++ src/main/java/com/cgreen/pitchconverter/PitchConverter.java
@@ -15,7 +15,7 @@ import com.cgreen.pitchconverter.util.Params;
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 
-@Command(name = "PitchConverter", mixinStandardHelpOptions = true, version = "0.5.8")
+@Command(name = "PitchConverter", mixinStandardHelpOptions = true, version = "0.6.0")
 public class PitchConverter implements Runnable {
     
     private static final Logger LOGGER = LogManager.getLogger();
