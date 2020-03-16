34bfaf70e69297b887f3a014309c918208114ba1
Christopher Green <cgvoice35@gmail.com>
2020-03-15 19:30:56 -0500

    Improve logging when building WordCollection

--- src/main/java/com/cgreen/pitchconverter/converter/Decoder.java
+++ src/main/java/com/cgreen/pitchconverter/converter/Decoder.java
@@ -3,16 +3,23 @@ package com.cgreen.pitchconverter.converter;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.cgreen.pitchconverter.datastore.WordCollection;
 import com.cgreen.pitchconverter.datastore.pitch.MusicSymbol;
 import com.cgreen.pitchconverter.util.FileReader;
 import com.cgreen.pitchconverter.util.Params;
 
 public final class Decoder {
+    private static final Logger LOGGER = LogManager.getLogger();
     public static Set<String> decodeMessage(Params p) {
         WordCollection wc = new WordCollection(p.getWordCollectionFile().getAbsolutePath());
-        if (!wc.buildWordCollection()) {
-            System.out.println("ERROR: No word collection found at " + p.getWordCollectionFile().getAbsolutePath());
+        if (wc.buildWordCollection()) {
+            // TODO: Give time
+            LOGGER.info("Words loaded successfully");
+        } else {            
+            LOGGER.fatal("No word collection found at " + p.getWordCollectionFile().getAbsolutePath());
             System.exit(1);
         }
         List<MusicSymbol> music = FileReader.getMusic(p.getInFile());
