2f5f4c031d1a2507ca2cd2cffa5095ee6401c560
Christopher Green <cgvoice35@gmail.com>
2020-03-22 21:33:26 -0500

    Add WordCollection build time logger and tracker bool

--- src/main/java/com/cgreen/pitchconverter/decoder/WordCollection.java
+++ src/main/java/com/cgreen/pitchconverter/decoder/WordCollection.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Collection of words used for the pitch converter to find valid words. Words
  * are obtained from a text file.
@@ -12,13 +15,17 @@ import java.util.Scanner;
  *
  */
 public class WordCollection {
-    private String filePath;
+    private static final Logger LOGGER = LogManager.getLogger();
     private static final int ALPHABET_SIZE = 26;
     private static WordNode root;
+    
+    private String filePath;
+    private boolean isBuilt;
     
     WordCollection(String filePath) {
         this.filePath = filePath;
         root = new WordNode();
+        isBuilt = false;
     }
     
     // trie node
@@ -65,9 +72,12 @@ public class WordCollection {
      * @throws FileNotFoundException
      */
     boolean buildWordCollection() {
-        // TODO: Don't build again if already built
         // TODO: Don't assume each line in the input file contains only a single word.
+        if (isBuilt) {
+            return false;
+        }
         Scanner s;
+        long start = System.currentTimeMillis();
         try {
             File file = new File(filePath);
             s = new Scanner(file);
@@ -78,6 +88,8 @@ public class WordCollection {
                 }
             }
             s.close();
+            LOGGER.debug("Word collection built in {} s.", (System.currentTimeMillis() - start) / 1000.);
+            isBuilt = true;
             return true;
         } catch (FileNotFoundException fnfe) {
             return false;
