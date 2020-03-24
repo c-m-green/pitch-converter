46eb41d771f166c9b5814cb016faafc585f2897b
Christopher Green <cgvoice35@gmail.com>
2020-03-23 22:37:09 -0500

    Refactor code from PitchDecoder into new StringSolver
    - StringSolver is a helper class to PitchDecoder that will take care of
    figuring out how a Pitch might convert into text.
    - Using StringSolver will allow for testing of its methods that were
    previously private within PitchDecoder.

--- src/main/java/com/cgreen/pitchconverter/decoder/PitchDecoder.java
+++ src/main/java/com/cgreen/pitchconverter/decoder/PitchDecoder.java
@@ -63,10 +63,10 @@ public class PitchDecoder {
             String possibleChars = "";
             switch(m) {
             case DEGREE:
-                possibleChars = getPossibleCharsByDegree(in.get(i), checkChromatic);
+                possibleChars = StringSolver.getPossibleCharsByDegree(in.get(i), checkChromatic);
                 break;
             case LETTER:
-                possibleChars = getPossibleCharsByLetter(in.get(i), useGermanH);
+                possibleChars = StringSolver.getPossibleCharsByLetter(in.get(i), useGermanH);
                 break;
             default:
                 // Should be unreachable
@@ -94,7 +94,7 @@ public class PitchDecoder {
                 }
             }
             //System.out.println("Current string: " + combo + " (" + (i + 1) + "/" + iterations + ")");
-            List<String> l = getPotentialStrings(combo, wc);
+            List<String> l = StringSolver.getPotentialStrings(combo, wc);
             for (String s : l) {
                 results.add(s);
             }
@@ -111,105 +111,6 @@ public class PitchDecoder {
             index = comboIndices.length - 1;
         }
         return results;
-    }
-
-    /**
-     * Searches a collection of words to identify valid words in an input string.
-     * 
-     * @param potentialLine - String to be read for words
-     * @param wc            - WordCollection
-     * @return - List of perfect and partial matches
-     */
-    private static List<String> getPotentialStrings(String potentialLine, WordCollection wc) {
-        // System.out.println("Searching for words in: \"" + potentialLine + "\"");
-        List<String> possibilities = new ArrayList<String>();
-        int head = 0;
-        int current = head + 1;
-        int foot = potentialLine.length();
-        while (current <= foot) {
-            String substr = potentialLine.substring(head, current);
-            if (wc.containsWord(substr)) {
-                // System.out.println("Found word: " + substr);
-                String newQuery = potentialLine.substring(substr.length(), foot);
-                if (newQuery.length() > 0) {
-                    // System.out.println("Now trying: " + newQuery);
-                    List<String> subResults = getPotentialStrings(newQuery, wc);
-                    if (subResults.isEmpty()) {
-                        possibilities.add(substr + "?");
-                    } else {
-                        for (String s : subResults) {
-                            // System.out.println("Adding to the results: " + substr + " " + s);
-                            possibilities.add(substr + " " + s);
-                        }
-                    }
-                } else {
-                    possibilities.add(substr);
-                }
-            } else {
-                // System.out.println("\"" + substr + "\" is not a word.");
-            }
-            current++;
-        }
-        // System.out.println("For " + potentialLine + ", returning " +
-        // possibilities.size() + " results.");
-        return possibilities;
-    }
-
-    /**
-     * Derive all possible characters that could have converted by degree to the
-     * input Pitch.
-     * 
-     * @param p           - Input Pitch
-     * @param isChromatic - Check for chromatic notes. If false, check members of
-     *                    the C Major scale
-     * @return String of potential characters
-     */
-    private static String getPossibleCharsByDegree(Pitch p, boolean isChromatic) {
-        if (p.getPitchClass() == '?') {
-            return "";
-        }
-        int bottomIndex = 97; // 'a'
-        int index;
-        if (isChromatic) {
-            index = bottomIndex + p.getPitchClassAsInt();
-        } else {
-            if (p.isNatural()) {
-                int pc = p.getPitchClassAsInt();
-                index = (pc < 5) ? bottomIndex + pc / 2 : bottomIndex + pc / 2 + 1;
-            } else {
-                return "";
-            }
-        }
-        String chars = "";
-        while (index < bottomIndex + 26) {
-            chars += (char) index + "";
-            int increase = (isChromatic) ? 12 : 7;
-            index += increase;
-        }
-        return chars;
-    }
-    
-    /**
-     * Derive all possible characters that could have converted by letter to the
-     * input Pitch.
-     * 
-     * @param p
-     * @param useGermanH
-     * @return String of potential characters
-     */
-    private static String getPossibleCharsByLetter(Pitch p, boolean useGermanH) {
-        if (p.getPitchClass() == '?') {
-            return "";
-        }
-        final String pitchClasses = useGermanH ? "9t02457e" : "9e02457";
-        int bottomIndex = 97; // 'a'
-        int index = pitchClasses.indexOf(p.getPitchClass()) + bottomIndex;
-        String chars = "";
-        while (index < bottomIndex + 26) {
-            chars += (char) index + "";
-            index += pitchClasses.length();
-        }
-        return chars;
     }
     
     // TODO: Move this somewhere else.
